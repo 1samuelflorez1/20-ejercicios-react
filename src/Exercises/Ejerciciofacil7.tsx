1f818031026f032d161e4b957aa7626bf1d7bbd6
samuel florez <samuel.f.v.t.p@gmail.com>
2025-09-25 17:32:55 -0500

    exercise 7 completed

--- src/Exercises/Ejerciciofacil7.tsx
+++ src/Exercises/Ejerciciofacil7.tsx
@@ -1,19 +1,28 @@
 // Implementa un formulario con campos de nombre y correo electrónico que 
 // muestre los datos ingresados en tiempo real debajo de los inputs.
 
+import { useState } from "react"
 import "../index.css"
 
 function FirstForm (){
+    const [texto1, setTexto1] = useState("")
+    const [texto2, setTexto2] = useState("")
 
     return (<>
-        <form>
+        <form className="form">
             <input 
+            className="inputejer7"
             placeholder="Name" 
-            type="text"></input>
+            type="text"
+            onChange={e => setTexto1(e.target.value)}></input>
+            <p>Nombre: {texto1}</p>
             
             <input 
+            className="inputejer7"
             placeholder="Email" 
-            type="email"></input>
+            type="email"
+            onChange={e => setTexto2(e.target.value)}></input>
+            <p>Email: {texto2}</p>
         </form>
     </>)
 }
