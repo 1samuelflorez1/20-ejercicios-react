f3d4482879e275f2fdebfe40c972cc2a4e533c4f
samuel florez <samuel.f.v.t.p@gmail.com>
2025-10-01 17:33:58 -0500

    notes

--- src/Exercises/Ejercicionormal19.tsx
+++ src/Exercises/Ejercicionormal19.tsx
@@ -28,13 +28,31 @@ function SumaApiArmas (){
         document.body.classList = "colorFondoArmas"
     }, [])
 
-    function clickBtn(gun: armasType){
-         if(compradas.includes(gun.uuid)){
+    function clickBtn(gun: armasType){   
+        //en esta parte se usa gun: armasType porque es lo que se esta usando y para poder usarlo, se tipea
+
+         if(compradas.includes(gun.uuid)){ 
             setCompradas(compradas.filter(id => id !== gun.uuid))
             setValor(valor - gun.shopData.cost)
+            //en este if pasa, 1: comprueba si en "compradas" se incluye el id de algun arma
+            //si esto es asi entonces este if tiene como objetivo quitarlo del array
+            //y restarle la cantidad, 2: en la segunda linea, lo que ocurre es que 
+            //con filter, se crea un nuevo array pero solo con la condicion que se le indique
+            //en este caso lo que se le indica es que creee el array con todos los id´s APARTE
+            //del que se coincide con el que se vendio, para poder quitarlo y 
+
+
          }else{
             setCompradas([...compradas, gun.uuid])
             setValor(valor + gun.shopData.cost)
+            //lo mas importante aqui son los tres puntos el cual se llama (spread operator)
+            //lo que significa que, crea un nuevo array, osea reemplaza el anterior, SUMANDOLE
+            //el nuevo id que se registro
+            //ejemplo:
+            // compradas = ["id1", "id2"]
+            // gun.uuid = "id3"
+            // [...compradas, gun.uuid] 
+            // // resultado: ["id1", "id2", "id3"]
          }
     }
 
@@ -46,7 +64,7 @@ function SumaApiArmas (){
 
             {armas.map((gun) => 
             {const estaComprada = compradas.includes(gun.uuid)
-                
+
             return (<div id="gunsola" key={gun.uuid}>
                     <img src={gun.displayIcon}></img>
                     <p id="namegun">{gun.displayName}</p>
