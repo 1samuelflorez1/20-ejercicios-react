3b0326ec85dbb5e878c3266a192e2e3d996025c1
samuel florez <samuel.f.v.t.p@gmail.com>
2025-10-02 17:37:34 -0500

    start final exercice

---
+++ src/Exercises/Ejercicionormal20.tsx
@@ -0,0 +1,11 @@
+// Implementa un gestor de tareas donde puedas agregar, marcar como 
+// completadas y filtrar entre "Todas", "Pendientes" y "Completadas".
+
+function GestorTareas (){
+
+    return(<>
+        
+    </>)
+}
+
+export default GestorTareas
